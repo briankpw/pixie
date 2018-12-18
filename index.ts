4180550615ae82b0d49d6ac9070a50542f5c6700
KOH,BRIAN (K-Singapore,ex1) <brian.koh@keysight.com>
2018-12-18 10:27:49 +0800

    PMATH Permanently Disabled

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './lib/pixie';
-export { TYPE, CONDITION, PMATH, Condition, Dimension, Measurement, Aggregate, PMath } from './lib/interface/aggregate';
+export { TYPE, CONDITION, Condition, Dimension, Measurement, Aggregate } from './lib/interface/aggregate';
 export { Sort, SORT } from './lib/interface/sort';
+export *  from './lib/util/pixie';
--- lib/model/pmath.ts
+++ lib/model/pmath.ts
@@ -2,39 +2,6 @@ import * as _ from 'underscore';
 import * as math from 'mathjs';
 import { ReplaceAll, IsNumber } from '../util/tool';
 
-export enum PMATH {
-  NONE,
-  SUM,
-  SUBTRACT,
-  MULTIPLE,
-  DIVIDE
-}
-
-export interface PMathInterface {
-  key: string;
-  pMath: PMATH;
-  isPackageStartEnd?: boolean;
-}
-
-export class PMath implements PMathInterface {
-  constructor(public key: string, public pMath: PMATH, public isPackageStartEnd?: boolean) {}
-}
-
-function CalculatingPMath(type: PMATH, value: number, lastCount: number) {
-  switch (type) {
-    case PMATH.NONE:
-      return lastCount;
-    case PMATH.SUM:
-      return lastCount + value;
-    case PMATH.SUBTRACT:
-      return lastCount - value;
-    case PMATH.MULTIPLE:
-      return lastCount * value;
-    case PMATH.DIVIDE:
-      return lastCount / value;
-  }
-}
-
 function Calculating(d: any, formula: string) {
   const splitPattern = /[\s()*/%+-]+/g;
   let formulaWithValue = formula;
